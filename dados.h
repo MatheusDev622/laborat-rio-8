0acc2ec8a16b6ecd40dfa2bb300de8aa2dcd17be
MatheusDev622 <168672066+MatheusDev622@users.noreply.github.com>
2025-07-14 22:00:19 -0300

    Delete dados.h

--- dados.h
+++
@@ -1,9 +0,0 @@
-#ifndef DADOS_H
-#define DADOS_H
-
-#include "estrutura.h"
-
-void salvarDados(informacoes_conta *conta, const char *nomeArquivo);
-void carregarDados(informacoes_conta *conta, const char *nomeArquivo);
-
-#endif
